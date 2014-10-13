715e32750d789204b9e58eeeba582e3291091ee1
Aj <btchaggler@gmail.com>
2014-10-13 15:28:34 +0000

    file for just testing out on ipython prompt

---
+++ just_imports.py
@@ -0,0 +1,19 @@
+from config import (KAYAKO_API_URL,
+                    KAYAKO_API_KEY,
+                    KAYAKO_SECRET_KEY,
+                    HF_API_URL,
+                    HF_API_KEY,
+                    HF_AUTH_KEY)
+from kayako import (KayakoAPI,
+                    User,
+                    Department,
+                    Ticket,
+                    TicketPost,
+                    TicketAttachment)
+import requests
+import shelve
+
+
+k_api = KayakoAPI(KAYAKO_API_URL, KAYAKO_API_KEY, KAYAKO_SECRET_KEY)
+h_api = (HF_API_KEY, HF_AUTH_KEY)
+
