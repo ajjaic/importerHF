799121d2c732810e4f915db7e215050d862f4a5f
Aj <btchaggler@gmail.com>
2014-10-20 11:10:05 +0000

    major refactoring and better help messages

--- main.py
+++ main.py
@@ -1,37 +1,56 @@
-from config import (KAYAKO_API_URL,#{{{
-                    KAYAKO_API_KEY,
-                    KAYAKO_SECRET_KEY,
-                    HF_API_URL,
-                    HF_API_KEY,
-                    HF_AUTH_KEY)
-from kayako import (KayakoAPI,
-                    User,
-                    Department,
-                    Ticket,
-                    Staff,
-                    TicketPost,
-                    TicketAttachment,
-                    TicketNote,
-                    TicketStatus,
-                    TicketPriority)
 import tempfile
 import os
-import requests
 import hashlib
 import random
 import base64
 import hmac
 import string
-import xmltodict
 import json
-import datetime#}}}
+import sys
+import datetime
+import re
 
-"""#{{{
+from config import (KAYAKO_API_URL,
+                    KAYAKO_API_KEY,
+                    KAYAKO_SECRET_KEY,
+                    HF_API_URL,
+                    HF_API_KEY,
+                    HF_AUTH_KEY)
+try:
+    import xmltodict
+except ImportError:
+    print "xmltodict not found"
+    print "try: pip install xmltodict"
+try:
+    import requests
+except ImportError:
+    print "requests not found"
+    print "try: pip install requests"
+
+try:
+    from kayako import (KayakoAPI,
+                        User,
+                        Department,
+                        Ticket,
+                        Staff,
+                        TicketPost,
+                        TicketAttachment,
+                        TicketNote,
+                        TicketStatus,
+                        TicketPriority)
+except ImportError:
+    print "kayako not found"
+    print "try: pip install kayako"
+
+
+PHONE_NUMBER_REGEXP = re.compile(r"^[\+]?\d{6,29}$")#{{{
+
+"""
 The handle kApi is for communicating with the
 Kayako API respectively.
-"""#}}}
+"""
 kApi = KayakoAPI(KAYAKO_API_URL, KAYAKO_API_KEY, KAYAKO_SECRET_KEY)
-hApi = (HF_API_KEY, HF_AUTH_KEY)
+hApi = (HF_API_KEY, HF_AUTH_KEY)#}}}
 
 def getKayakoSignature():#{{{
     """
@@ -65,9 +84,9 @@ def postToHappyFox(endpoint, payload, headers=None, files=None):#{{{
                     data=payload,
                     files=files,
                     auth=(HF_API_KEY, HF_AUTH_KEY))
-    return response
+    return response#}}}
 
-def getFromHappyFox(endpoint):
+def getFromHappyFox(endpoint):#{{{
     return requests.get(HF_API_URL + endpoint, auth=hApi)#}}}
 
 def getFromKayako(endpoint, **args):#{{{
@@ -80,7 +99,7 @@ def getFromKayako(endpoint, **args):#{{{
 
 
 class Tickets(object):
-    def __init__(self, hf_contacts,
+    def __init__(self, hf_contacts,#{{{
                     hf_categories,
                     hf_staff,
                     hf_status,
@@ -90,7 +109,10 @@ class Tickets(object):
                     k_tickets,
                     k_status,
                     k_priority,
-                    k_departments):
+                    k_departments,
+                    fromticket,
+                    len_kayako_tickets,
+                    logfile):
 
         self.hf_contacts = hf_contacts
         self.hf_categories = hf_categories
@@ -105,10 +127,12 @@ class Tickets(object):
         self.k_priority = k_priority
         self.k_departments = k_departments
 
-    def hCreateAllTickets(self):
+        self.fromticket = fromticket
+        self.len_kayako_tickets = len_kayako_tickets
+        self.logfile = logfile#}}}
 
-        len_kayako_tickets = len(self.k_tickets)
-        for i, t in enumerate(self.k_tickets, 1):
+    def hCreateAllTickets(self):
+        for i, t in enumerate(self.k_tickets, self.fromticket):
             print "Getting ticket updates..",
             kayako_ticketupdates = kApi.get_all(TicketPost, t.id)
             try:
@@ -197,46 +221,48 @@ class Tickets(object):
                 hf_pvt_notes.append(hf_pvt_note)
 
 
-            #Post the ticket first
-            #print "Posting ticket {0}/{1}".format(i, len_kayako_tickets)
-            print "Posting ticket..",
-            endpoint = 'tickets/'
-            if happyfox_attachments.has_key(first_update.id):
-                response = postToHappyFox(endpoint, hf_tkt, files=happyfox_attachments[first_update.id])
-            else:
-                response = postToHappyFox(endpoint, hf_tkt)
-            ticketid = response.json()['id']
-
-            #Post update to set ticket status and assignee to kayako status and assignee
-            #status=self.hf_status[self.k_status[t.statusid]]
-            #assignee = self.hf_staff[self.k_staff[t.ownerstaffid]]
-            #status_assignee_update = dict(status=status, staff=self.hf_admin['id'], assignee=assignee)
-            endpoint = 'ticket/{0}/staff_update/'.format(ticketid)
-            postToHappyFox(endpoint, status_assignee_update)
-
-            #Now post the tickets updates
-            #print "Posting updates of ticket {0}/{1}".format(i, len_kayako_tickets)
-            print "updates..",
-            for u in hf_tkt_updates:
-                if u.has_key('staff'):
-                    endpoint = 'ticket/{0}/staff_update/'.format(ticketid)
+            try:
+                #Post the ticket first
+                print "Posting ticket..",
+                endpoint = 'tickets/'
+                if happyfox_attachments.has_key(first_update.id):
+                    response = postToHappyFox(endpoint, hf_tkt, files=happyfox_attachments[first_update.id])
                 else:
-                    endpoint = 'ticket/{0}/user_reply/'.format(ticketid)
-                uid = u['id']
-                del u['id']
-                if happyfox_attachments.has_key(uid):
-                    response = postToHappyFox(endpoint, u, files=happyfox_attachments[uid])
-                else:
-                    postToHappyFox(endpoint, u)
+                    response = postToHappyFox(endpoint, hf_tkt)
+                ticketid = response.json()['id']
 
-            #Now post all the private notes
-            #print "Posting private notes of ticket {0}/{1}".format(i, len_kayako_tickets)
-            print "notes..",
-            for pn in hf_pvt_notes:
-                endpoint = 'ticket/{0}/staff_pvtnote/'.format(ticketid)
-                postToHappyFox(endpoint, pn)
+                #Post update to set ticket status and assignee to kayako status and assignee
+                #status=self.hf_status[self.k_status[t.statusid]]
+                #assignee = self.hf_staff[self.k_staff[t.ownerstaffid]]
+                #status_assignee_update = dict(status=status, staff=self.hf_admin['id'], assignee=assignee)
+                endpoint = 'ticket/{0}/staff_update/'.format(ticketid)
+                postToHappyFox(endpoint, status_assignee_update)
 
-            print "Completed {0}/{1}".format(i, len_kayako_tickets)
+                #Now post the tickets updates
+                print "updates..",
+                for u in hf_tkt_updates:
+                    if u.has_key('staff'):
+                        endpoint = 'ticket/{0}/staff_update/'.format(ticketid)
+                    else:
+                        endpoint = 'ticket/{0}/user_reply/'.format(ticketid)
+                    uid = u['id']
+                    del u['id']
+                    if happyfox_attachments.has_key(uid):
+                        response = postToHappyFox(endpoint, u, files=happyfox_attachments[uid])
+                    else:
+                        postToHappyFox(endpoint, u)
+
+                #Now post all the private notes
+                print "notes..",
+                for pn in hf_pvt_notes:
+                    endpoint = 'ticket/{0}/staff_pvtnote/'.format(ticketid)
+                    postToHappyFox(endpoint, pn)
+
+                print "Completed {0}/{1}".format(i, self.len_kayako_tickets)
+            except:
+                errmsg = "Ticket with Kayako display id {0} has failed".format(t.displayid)
+                print errmsg
+                self.logfile.write(errmsg+'\n')
 
 def create_temporary_file(filename, contents):
     path = os.path.join(tempfile.gettempdir(), "KayakoImport", HF_API_KEY) + os.sep
@@ -262,6 +288,7 @@ def hGetAllStaff():
             admin = s
     return happyfox_staff, admin
 
+#TODO: Fix contact custom field
 def hGetAllContacts(k_users):
     def gen_chunks(l, n):
         for i in range(0, len(l), n):
@@ -270,8 +297,9 @@ def hGetAllContacts(k_users):
     endpoint = 'users/'
     happyfox_contacts = dict()
     for kusers in gen_chunks(k_users, 100):
+        json_kusers = json.dumps(kusers)
         response = postToHappyFox(endpoint,
-                        json.dumps(kusers),
+                        json_kusers,
                         headers={"Content-Type": "application/json"})
         for contact in response.json():
             happyfox_contacts[contact['email']] = contact['id']
@@ -303,6 +331,7 @@ def kGetAllStatus():
         kayako_status[s.id] = s.title
     return kayako_status
 
+#TODO: Fix contact custom field
 def kGetAllUsers():
     endpoint = '/Base/User/Filter'
     response = getFromKayako(endpoint)
@@ -314,18 +343,30 @@ def kGetAllUsers():
         emails = ku['email']
         ku_emails = emails.split(',') if type(ku['email']) != list else emails
         for email in ku_emails:
-            kayako_users.append(dict(name=ku['fullname'], email=email))
+            newuser = dict(name=ku['fullname'], email=email)
+            userphone = ku['phone']
+            if userphone:
+                userphone = re.sub('[- .]', '', userphone)
+                if not PHONE_NUMBER_REGEXP.match(userphone):
+                    newuser['c-cf-1'] = userphone
+                else:
+                    newuser['phones'] = [dict(type='m', number=userphone)]
+            kayako_users.append(newuser)
     return kayako_users
 
-def kGetAllTicketsAndDepartments(k_status):
+def kGetAllDepartments():
     depts = kApi.get_all(Department)
-    kayako_tickets = list()
     kayako_departments = dict()
     for d in depts:
+        kayako_departments[d.id] = d.title
+    return kayako_departments
+
+def kGetAllTickets(k_status, k_departments):
+    kayako_tickets = list()
+    for d in k_departments:
         for s in k_status.keys():
             kayako_tickets.extend(kApi.get_all(Ticket, d.id, ticketstatusid=s))
-        kayako_departments[d.id] = d.title
-    return kayako_departments, kayako_tickets
+    return kayako_tickets
 
 def kGetAllStaff():
     staff = kApi.get_all(Staff)
@@ -342,44 +383,132 @@ def kGetTicketAttachment(ticketid, attachmentid):
     response.close()
     return (fn, ft, c)
 
+def in_sync(k_dict, hf_dict):
+    need_to_create = list()
+    for kd in k_dict.values():
+        if not hf_dict.get(kd, None):
+            need_to_create.append(kd)
+
+    return need_to_create
+
+def requiredHFobjects(objects, plural):
+    print "The following {0} need to be created on Happyfox".format(plural)
+    for o in objects:
+        print o
+
 def newmain():
-    k_users = kGetAllUsers()
+    #print "All of these must be manually created on the happyfox instance before running this importer"
+    #print " - Categories (All categories must be public)"
+    #print " - Staff (All Staff must have different names)"
+    #print " - Status"
+    #print " - Priority"
+    #print " - Ticket custom field for kayako display id (Type: Text)"
+    #print ""
+
+    #print "Start from (b)eginning or (c)ontinue from where you left off?"
+    #start_or_continue = raw_input()
+    #if start_or_continue != 'b' or start_or_continue != 'c':
+        #sys.exit()
+    #if start_or_continue == 'b':
+        #fromticket = 1
+    #else:
+        #if start_or_continue == 'c' and len(sys.argv) == 2:
+            #fromticket = int(sys.argv[1])
+        #else:
+            #print "Enter ticket number from where to start"
+
+    #if raw_input("Proceed(y/n)?") != 'y':
+        #sys.exit()
+    #fromticket = 1
+    #if len(sys.argv) == 2:
+        #fromticket = int(sys.argv[1])
+    """
+    Change current directory to the directory of this script
+    """
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+    """
+    Kayako Departments and Happyfox Categories
+    """
+    print "Getting Kayako departments"
+    k_departments = getFromShelve('k_departments')
+    if not k_departments:
+        k_departments = kGetAllDepartments()
+        putInShelve('k_departments', k_departments)
+    hf_categories = hGetAllCategories()
+    to_sync = in_sync(k_departments, hf_categories):
+    if not to_sync:
+        requiredHFobjects(to_sync, 'categories')
+        sys.exit()
+
+    """
+    Kayako and Happyfox Staff
+    """
+    print "Getting Kayako staff"
+    k_staff = getFromShelve('k_staff')
+    if not k_staff:
+        k_staff = kGetAllStaff()
+        putInShelve('k_staff', k_staff)
+    hf_staff, hf_admin = hGetAllStaff()
+    to_sync = in_sync(k_staff, hf_staff)
+    if not to_sync:
+        requiredHFobjects(to_sync, 'staff members')
+        sys.exit()
+
+    """
+    Kayako and Happyfox Statuses
+    """
+    print "Getting Kayako statuses"
+    k_status = getFromShelve('k_status')
+    if not k_status:
+        k_status = kGetAllStatus()
+        putInShelve('k_status', k_status)
+    hf_status = hGetAllStatus()
+    to_sync = in_sync(k_status, hf_status)
+    if not to_sync:
+        requiredHFobjects(to_sync, 'statuses')
+        sys.exit()
+
+    """
+    Kayako and Happyfox Priorities
+    """
+    print "Getting Kayako priorities"
+    k_priority = getFromShelve('k_priority')
+    if not k_priority:
+        k_priority = kGetAllPriority()
+        putInShelve('k_priority', k_priority)
+    hf_priority = hGetAllPriority()
+    to_sync = in_sync(k_priority, hf_priority)
+    if not to_sync:
+        requiredHFobjects(to_sync, 'priorities')
+        sys.exit()
+
+    """
+    Kayako and Happyfox Users/Contacts
+    """
+    print "Getting Kayako users"
+    k_users = getFromShelve('k_users')
+    if not k_users:
+        k_users = kGetAllUsers()
+        putInShelve('k_users', k_users)
     hf_contacts = hGetAllContacts(k_users)
 
-    hf_categories = hGetAllCategories()
-    hf_staff, hf_admin = hGetAllStaff()
-    hf_status = hGetAllStatus()
-    hf_priority = hGetAllPriority()
+    """
+    Kayako Tickets
+    """
+    print "Getting Kayako tickets"
+    k_tickets = getFromShelve('k_tickets')
+    if not k_tickets:
+        k_tickets = kGetAllTickets(k_status, k_departments)
+        putInShelve('k_tickets', k_tickets)
 
-    k_staff = kGetAllStaff()
-    k_status = kGetAllStatus()
-    k_priority = kGetAllPriority()
-    k_departments, k_tickets = kGetAllTicketsAndDepartments(k_status)
 
-    #putinshelve('hf_contacts', hf_contacts)
-    #putinshelve('hf_categories', hf_categories)
-    #putinshelve('hf_staff', hf_staff)
-    #putinshelve('hf_status', hf_status)
-    #putinshelve('hf_priority', hf_priority)
-    #putinshelve('hf_admin', hf_admin)
-    #putinshelve('k_staff', k_staff)
-    #putinshelve('k_status', k_status)
-    #putinshelve('k_priority', k_priority)
-    #putinshelve('k_departments', k_departments)
-    #putinshelve('k_tickets', k_tickets)
+    #len_kayako_tickets = len(k_tickets)
+    #if fromticket > 1:
+        #k_tickets = k_tickets[fromticket:]
 
-    #hf_contacts = getfromshelve('hf_contacts')
-    #hf_categories = getfromshelve('hf_categories')
-    #hf_staff = getfromshelve('hf_staff')
-    #hf_status = getfromshelve('hf_status')
-    #hf_priority = getfromshelve('hf_priority')
-    #hf_admin = getfromshelve('hf_admin')
-    #k_staff = getfromshelve('k_staff')
-    #k_status = getfromshelve('k_status')
-    #k_priority = getfromshelve('k_priority')
-    #k_departments = getfromshelve('k_departments')
-    #k_tickets = getfromshelve('k_tickets')
 
+    logfile = open('./kayako.log', 'a')
     Tickets(hf_contacts,
                 hf_categories,
                 hf_staff,
@@ -390,19 +519,28 @@ def newmain():
                 k_tickets,
                 k_status,
                 k_priority,
-                k_departments).hCreateAllTickets()
+                k_departments,
+                fromticket,
+                len_kayako_tickets,
+                logfile).hCreateAllTickets()
+    logfile.close()
 
-def getfromshelve(varname):
+def getFromShelve(varname):#{{{
+    """
+    Returns `None` if varname is not found
+    """
     import shelve
-    s = shelve.open('../main.hg/tempdata')
-    varvalue = s[varname]
+    #s = shelve.open('../main.hg/tempdata')
+    s = shelve.open('./tempdata')
+    varval = s.get(varname, None)
     s.close()
     return varvalue
 
-def putinshelve(varname, varvalue):
+def putInShelve(varname, varvalue):
     import shelve
-    s = shelve.open('../main.hg/tempdata')
+    s = shelve.open('./tempdata')
     s[varname] = varvalue
-    s.close()
+    s.close()#}}}
+
 
 newmain()
